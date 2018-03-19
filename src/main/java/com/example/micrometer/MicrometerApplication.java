58a80d4140af9ec34ad416ff1437d87e796a9a8d
Josh Long <starbuxman@gmail.com>
2018-03-19 16:37:46 -0700

    take12

--- src/main/java/com/example/micrometer/MicrometerApplication.java
+++ src/main/java/com/example/micrometer/MicrometerApplication.java
@@ -1,6 +1,5 @@
 package com.example.micrometer;
 
-import io.micrometer.core.annotation.Timed;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Timer;
 import io.micrometer.core.instrument.config.MeterFilter;
@@ -10,9 +9,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Duration;
 import java.util.concurrent.Executors;
@@ -22,56 +18,35 @@ import java.util.concurrent.TimeUnit;
 @SpringBootApplication
 public class MicrometerApplication {
 
+	private final ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+
 	public static void main(String[] args) {
 		SpringApplication.run(MicrometerApplication.class, args);
 	}
 
-	private final ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
-
-	/*
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> registryCustomizer(@Value("${REGION:us-west}") String region) {
+		return registry -> registry.config().commonTags("region", region);
+	}
 
 	@Bean
 	MeterFilter meterFilter() {
-		return MeterFilter.accept(); //.denyNameStartsWith("jvm");
-	}
-*/
-
-	@Bean
-	MeterRegistryCustomizer<MeterRegistry> custom(@Value("${my-app.region:us-west}") String region) {
-		return new MeterRegistryCustomizer<MeterRegistry>() {
-			@Override
-			public void customize(MeterRegistry registry) {
-				registry.config().commonTags("us-region", region);
-			}
-		};
+		return MeterFilter.denyNameStartsWith("jvm");
 	}
 
 	@Bean
-	ApplicationRunner run(MeterRegistry mr) {
+	ApplicationRunner runner(MeterRegistry mr) {
 		return args ->
-				this.executorService.scheduleAtFixedRate(() -> {
-					String region = Math.random() > .5 ? "us-east" : "us-west";
-
-					long min = (long) Math.min(Math.random() * 60, 60);
-					Duration ofSeconds = Duration.ofSeconds(min);
-					Timer.builder("st-task")
-							.tag("region", region)
-							.publishPercentiles(.5, .95)
-							.sla(Duration.ofMillis(1), Duration.ofSeconds(10))
-							.register(mr)
-							.record(ofSeconds);
-
-
-				}, 500, 500, TimeUnit.MILLISECONDS);
+				this.executorService
+						.scheduleWithFixedDelay(
+								() ->
+										Timer
+												.builder("transform-photo-task")
+												.sla(Duration.ofMillis(1), Duration.ofSeconds(10))
+												.publishPercentileHistogram()
+												.tag("format", Math.random() > .5 ? "png" : "jpg")
+												.register(mr)
+												.record(Duration.ofMillis((long) (Math.random() * 1000))), 500, 500, TimeUnit.MILLISECONDS);
 	}
 }
 
-@Timed(extraTags = {"region", "us-east"})
-@RestController
-class GreetingsRestController {
-
-	@GetMapping("/hi/{name}")
-	String hi(@PathVariable String name) {
-		return "Hello, " + name + "!";
-	}
-}
